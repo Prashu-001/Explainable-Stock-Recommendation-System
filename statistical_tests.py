9da1e6588093099259cce54976ce2a1a2f485020
Prashu-001 <prashu150603@gmail.com>
2025-11-23 15:02:53 +0530

    Update statistical_tests.py

--- statistical_tests.py
+++ statistical_tests.py
@@ -5,24 +5,24 @@ from scipy.stats import linregress
 import pandas as pd
 import numpy as np
 
-def adf_test(series: pd.Series):
+def adf_test(series):
     res = adfuller(series.dropna(), autolag = 'AIC')
     return {'adf_stat':res[0],'pvalue':res[1],'usedlag':res[2],'nobs':res[3]}
 
-def engle_arch_test(series: pd.Series,lags: int = 5):
+def engle_arch_test(series, lags = 5):
     res = het_arch(series.dropna(),nlags = lags)
     return {'lm_stat':res[0],'lm_pvalue':res[1],'f_stat':res[2],'f_pvalue':res[3]}
 
-def ljung_box_test(series: pd.Series, lags = [10]):
+def ljung_box_test(series, lags = [10]):
     res = acorr_ljungbox(series.dropna(),lags = lags, return_df = True)
     return {'lb_stat':res['lb_stat'].iloc[-1],'lb_pvalue':res['lb_pvalue'].iloc[-1]}
 
-def stl_decompose(series: pd.Series,period: int):
+def stl_decompose(series, period):
     stl = STL(series.dropna(),period = period,robust=True)
     res = stl.fit()
     return {'trend': res.trend, 'seasonal': res.seasonal, 'resid': res.resid}
 
-def trend_strength(series: pd.Series):
+def trend_strength(series):
     y = series.dropna().values
     x = np.arange(len(y))
     if len(y)< 3:
